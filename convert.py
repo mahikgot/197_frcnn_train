e14f4be59adc8cbc0a907d144142f210497de179
Mark Guiang <mahikgot@gmail.com>
2022-04-25 22:28:46 +0800

    change convert filename output

--- convert.py
+++ convert.py
@@ -73,6 +73,6 @@ def convert(json_fname, label_fname):
     output['categories'] = get_categories()
 
     return output
-with open('./dataset/annotations/labels_train.json', 'w') as outfile:
+with open('./dataset/annotations/instances_train.json', 'w') as outfile:
     json.dump(convert('./dataset/data/segmentation_train.json', './dataset/data/labels_train.csv'), outfile)
 
