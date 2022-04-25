a9c46ae039e37db57a52c0aa9e8e1415c8523960
Mark Guiang <mahikgot@gmail.com>
2022-04-25 22:35:46 +0800

    add debug print

--- reference/coco_utils.py
+++ reference/coco_utils.py
@@ -235,6 +235,7 @@ def get_coco(root, image_set, transforms, mode="instances"):
     transforms = T.Compose(t)
 
     img_folder, ann_file = PATHS[image_set]
+    print(img_folder)
     img_folder = os.path.join(root, img_folder)
     ann_file = os.path.join(root, ann_file)
 
