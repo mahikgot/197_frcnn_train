6e7eb13e0f0a8cbf65b12fef246f0ce5cc5f4f89
Mark Guiang <mahikgot@gmail.com>
2022-04-30 05:38:37 +0800

    optimize for less ram usage and load time when input is .mp4

--- infer.py
+++ infer.py
@@ -1,118 +1,138 @@
 import torchvision
 import numpy as np
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, IterableDataset
 from PIL import Image, ImageDraw, ImageOps
 import argparse
 from pathlib import Path
 import cv2
 from collections import deque
 import time
+import shutil
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input-path', default='./input', type=str, dest='path')
+parser.add_argument('--new-fps', default='24', type=int, dest='new_fps')
+parser.add_argument('--clean', default=False, type=bool, dest='clean')
+parser.add_argument('--batch-size', default=2, type=int, dest='batch_size')
+
 args = parser.parse_args()
 
-class Input(Dataset):
-    def __init__(self, data, transform=None):
-        self.data = data
+class Vid_Input(Dataset):
+    def __init__(self, vid, transform):
+        super(Vid_Input).__init__()
+        self.vid = vid
+        self.transform = transform
+
+    def __getitem__(self, idx):
+        success, frame = self.vid.read()
+        if success:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image = Image.fromarray(frame)
+            return self.transform(image).to(torch.device('cuda:0'))
+
+    def __len__(self):
+        return int(self.vid.get(cv2.CAP_PROP_FRAME_COUNT))
+
+class Img_Input(Dataset):
+    def __init__(self, image, transform):
+        self.image = image
         self.transform = transform
     def __getitem__(self, index):
-        return transform(self.data[index]).to(torch.device('cuda:0'))
+        return self.transform(self.image[index]).to(torch.device('cuda:0'))
+
     def __len__(self):
-        return len(self.data)
+        return len(self.image)
 
-def convert_to_img_data(vid):
+def convert_to_img_data(vid, new_fps):
     success, frame = vid.read()
-    fps = vid.get(cv2.CAP_PROP_FPS)
+    old_fps = vid.get(cv2.CAP_PROP_FPS)
+    fps = old_fps/new_fps
     images=[]
+    frame_number = 0
     while success:
-        converted = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image = Image.fromarray(converted)
+        image = Image.fromarray(frame)
         images.append(image)
+        vid.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
         success, frame = vid.read()
+        frame_number += fps
 
-    return images, fps
+    return images, new_fps
 
 def list_to_mp4(frames, fps, name):
     videodims = frames[0].size
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     video = cv2.VideoWriter('./output/' + name[:-4] + '_out.mp4', fourcc, fps, videodims)
     for i in frames:
-        video.write(cv2.cvtColor(np.array(i), cv2.COLOR_RGB2BGR))
+        video.write(np.array(i))
     video.release()
 
 
-def infer(model, dataloader, path_list, data_list, vid=False, fps=0):
-    img_ctr = 0
+def infer(model, dataloader, path_list, vid=False, fps=None):
     frames = deque([])
-    if data_list:
-        width, height = data_list[img_ctr].size
-        rx = width/640
-        ry = height/480
-        ray = [rx,ry,rx,ry]
-    whole = time.time()
+    img_ctr = 0
     for batch in dataloader:
-        start = time.time()
         preds = model(batch)
-        end = time.time()
-        print('model:', end-start)
         labels = [[pred["labels"].cpu().detach().numpy(), pred["scores"].cpu().detach().numpy()] for pred in preds]
         boxes = [pred["boxes"].cpu().detach().numpy() for pred in preds]
 
-        start = time.time()
+        transform = torchvision.transforms.ToPILImage()
         for img_idx, img in enumerate(boxes):
+            image = transform(batch[img_idx])
             for cat_idx, cat in enumerate(labels[img_idx][0]):
                 if labels[img_idx][1][cat_idx] >= 0.98:
-                    bbox = np.multiply(img[cat_idx], ray)
+                    bbox = img[cat_idx]
                     if cat == 1:
                         color = 'blue'
                         text = 'summit'
+
                     elif cat == 2:
                         color = 'red'
                         text  = 'coke'
+
                     else:
                         color = 'yellow'
                         text = 'juice'
-                    ImageDraw.Draw(data_list[img_ctr]).rectangle(bbox.tolist(), outline=color, width=2)
-                    ImageDraw.Draw(data_list[img_ctr]).text((bbox[0], bbox[1]), text=text, fill='black')
+
+                    ImageDraw.Draw(image).rectangle(bbox, outline=color, width=2)
+                    ImageDraw.Draw(image).text((bbox[0], bbox[1]), text=text, fill='black')
 
             if vid:
-                frames.append(data_list[img_ctr])
+                frames.append(image)
             else:
-                data_list[img_ctr].save('./output/' + path_list[img_ctr].name[:-4] + '_out.jpg')
+                image.save('./output/' + path_list[img_ctr][:-4] + '_out.jpg')
             img_ctr += 1
-        end = time.time()
-        print('draw:', end-start)
-    whole_end = time.time()
-    print('whole:', whole_end-whole)
 
-    huli = time.time()
     if vid:
         list_to_mp4(frames, fps, path_list)
-        huli_last = time.time()
-        print('output', huli_last-huli)
 
-transform = torchvision.transforms.Compose([torchvision.transforms.Resize((480, 640)), torchvision.transforms.ToTensor()])
+if __name__=='__main__':
+    begin = time.time()
+    img_transform = torchvision.transforms.Compose([torchvision.transforms.Resize(480), torchvision.transforms.ToTensor()])
+    img_path = [f for f in Path(args.path).glob('*.jpg')]
+    img_name = [f.name for f in img_path]
+    img_data = [Image.open(f) for f in img_path]
+    img_loader = DataLoader(dataset=Img_Input(img_data, transform), batch_size=args.batch_size)
 
-img_data = [Image.open(f) for f in Path(args.path).glob('*.jpg')]
-img_name = [f for f in Path(args.path).glob('*.jpg')]
-img_loader = DataLoader(dataset=Input(img_data, transform), batch_size=2)
+    vid_transform = torchvision.transforms.Compose([torchvision.transforms.Resize(480), torchvision.transforms.ToTensor()])
+    vids_name = [f for f in Path(args.path).glob('*.mp4')]
+    vids_data = [cv2.VideoCapture(args.path+ '/' + f.name) for f in vids_name]
+    vids_loader = [[DataLoader(dataset=Vid_Input(vid, transform), batch_size=args.batch_size)] for vid in vids_data]
 
-vids_data = [cv2.VideoCapture(args.path+ '/' + f.name) for f in Path(args.path).glob('*.mp4')]
-vids_name = [f for f in Path(args.path).glob('*.mp4')]
-frames_list = [convert_to_img_data(vid) for vid in vids_data]
-vids_loader = [[DataLoader(dataset=Input(frames[0], transform), batch_size=2), frames[1]] for frames in frames_list]
+    checkpoint = torch.load('checkpoint.pth')
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(num_classes=4, weights_backbone='ResNet50_Weights.IMAGENET1K_V1')
+    model.load_state_dict(checkpoint['model'])
+    model.to(torch.device('cuda:0'))
+    model.eval()
 
-checkpoint = torch.load('checkpoint.pth')
-model = torchvision.models.detection.fasterrcnn_resnet50_fpn(num_classes=4, weights_backbone='ResNet50_Weights.IMAGENET1K_V1')
-model.load_state_dict(checkpoint['model'])
-model.to(torch.device('cuda:0'))
-model.eval()
 
-print('tang')
-Path('./output').mkdir(parents=True, exist_ok=True)
-with torch.no_grad():
-    infer(model, img_loader, img_name, img_data)
-    for idx, loader in enumerate(vids_loader):
-        infer(model, loader[0], vids_name[idx].name,  frames_list[idx][0], fps=loader[1], vid=True)
+    if args.clean:
+        shutil.rmtree('./output', ignore_errors=True)
+    Path('./output').mkdir(parents=True, exist_ok=True)
+    with torch.no_grad():
+        infer(model, img_loader, img_name)
+        for idx, loader in enumerate(vids_loader):
+            infer(model, loader[0], vids_name[idx].name, vid=True, fps=args.new_fps)
+    end = time.time()
+    print(end-begin)
