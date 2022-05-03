8045ec62ce6ba123c48c83beaee9a04dc4a9cfa5
Mark Guiang <mahikgot@gmail.com>
2022-05-03 11:51:34 +0800

    add train.py

---
+++ train.py
@@ -0,0 +1,4 @@
+import subprocess
+
+subprocess.run(['python', 'detection/train.py', '--resume', 'checkpoint.pth','--data-path', './dataset', '--model', 'fasterrcnn_resnet50_fpn', '--workers', '1', '--lr', '0.0025', '--weights-backbone', 'ResNet50_Weights.IMAGENET1K_V1', '--epochs', '27'])
+
