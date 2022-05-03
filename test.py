94730a2f46bbb4210744d7f09d5bc01fbb187cb1
Mark Guiang <mahikgot@gmail.com>
2022-05-03 12:26:26 +0800

    upgrade pip to nightly

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ def download():
     print('Done downloading')
 
 subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
+supbprocess.run(['pip', 'install', '--pre', 'torch', 'torchvision', '--extra-index-url', 'https://download.pytorch.org/whl/nightly/cu113', '-U'])
 download()
 subprocess.run(['python', 'convert.py'])
 subprocess.run(['python', 'detection/train.py', '--resume', 'checkpoint.pth','--test-only', '--data-path','./dataset', '--model', 'fasterrcnn_resnet50_fpn', '--workers', '1', '--lr', '0.0025', '--weights-backbone', 'ResNet50_Weights.IMAGENET1K_V1'])
