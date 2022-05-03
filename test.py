c09d4b749bf2cb54b9dd65bffed917a926d33613
Mark Guiang <mahikgot@gmail.com>
2022-05-03 11:36:40 +0800

    add convert.py in test.py

--- test.py
+++ test.py
@@ -12,6 +12,7 @@ def download():
 
 subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
 download()
+subprocess.run(['python', 'convert.py'])
 subprocess.run(['python', 'detection/train.py', '--resume', 'checkpoint.pth','--test-only', '--data-path','./dataset', '--model', 'fasterrcnn_resnet50_fpn', '--workers', '1', '--lr', '0.0025', '--weights-backbone', 'ResNet50_Weights.IMAGENET1K_V1'])
 
 
